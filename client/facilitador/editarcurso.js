f3ab1e389ef730015edf79b50135d262f80c8bc6
vademar <amigo_diablito@hotmail.com>
2017-07-18 15:16:01 -0400

    edicion de curso

---
+++ client/facilitador/editarcurso.js
@@ -0,0 +1,28 @@
+var c=new ReactiveVar();
+
+Template.editarcurso.onRendered(function(){
+    var idre= FlowRouter.getParam('id');
+   // console.log(idre);
+    c.set(idre);
+    return true;
+});
+
+Template.editarcurso.events({
+  	'submit form': function(e) {
+    e.preventDefault();
+    id=c.get();
+    var a={
+    	"nombre":e.target.nomb.value,
+    	"descripcion":e.target.descri.value
+    }
+    Meteor.call('editarcu',id,a);
+    console.log(a);
+  }
+});
+
+Template.editarcurso.helpers({
+	editcurs(){
+		console.log(Cursos.find({_id:c.get()}).fetch());
+		return Cursos.find({_id:c.get()}).fetch();	
+	}
+});
